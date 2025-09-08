b6211ab8860c7ff487d6e1278e29a2d4a9eec866
Jota Santos <jailsonsantosdpge@outlook.com>
2025-09-08 14:27:25 -0400

    Criando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import NavMain from './components/Nav'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavMain />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Button from "./Button";
+
+const NavMain = () => {
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    alert('Função definida no componente pai');
+    setCount(count + 1)
+  }
+
+
+  return (
+    <>
+      <nav>
+        <a href="#">Navbar</a>
+        <Button label={'Action'} onClick={handleClick} />
+        <Button label={'Action 2'} onClick={handleClick} />
+      </nav>
+    </>
+  );
+};
+
+export default NavMain;
